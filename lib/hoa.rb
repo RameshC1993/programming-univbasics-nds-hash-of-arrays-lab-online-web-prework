88e9e1892b064801cf12d803336ca8f3d54f8f7d
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-01-13 18:49:54 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -9,7 +9,7 @@ def add_character(show, name)
   # Write your implementation here
   # Should return the array of the 'show' argument
   
-  BASE_HOA[:third_earthers] << name
+  BASE_HOA[:third_earthers] << show
   
 end
 
